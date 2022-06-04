e7e49b3b25bb0eed64904fc2fc9900ae88b87008
gids-dadi <gideons564@gmail.com>
2022-06-04 13:16:07 +0100

    Add Categories file

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import '../style/Categories.css';
 
 function Categories() {
-  return <div>Categories</div>;
+  return (
+    <div>
+      <button type='submit'>Check Status</button>
+    </div>
+  );
 }
 
 export default Categories;
