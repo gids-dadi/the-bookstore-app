160297b758cdf0683ad24707a6957eded993b2c6
gids-dadi <gideons564@gmail.com>
2022-06-04 13:29:35 +0100

    Fix the linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../style/Book.css';
 
 function Book(props) {
   const { items } = props;
 
   return (
-    <div className="book-container">
+    <div className='book-container'>
       <h1>Stored Books</h1>
       {items.map((book) => (
-        <div className="book" key={book.id}>
+        <div className='book' key={book.id}>
           &quot;
           {book.title}
           &quot; by &nbsp;
           {book.author}
-          <button>Remove</button>
+          <button type='button'>Remove</button>
         </div>
       ))}
     </div>
   );
 }
+
+Book.propTypes = {
+  data: PropTypes.isRequired,
+};
 
 export default Book;
