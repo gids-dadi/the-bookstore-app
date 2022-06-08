d677c1b230138823ed74712e75c6a081c995976f
gids-dadi <gideons564@gmail.com>
2022-06-08 10:08:19 +0100

    Clear linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -14,18 +14,18 @@ const Book = (props) => {
   };
 
   return (
-    <div className='book-container'>
-      <div className='block-l'>
-        <div className='info'>
+    <div className="book-container">
+      <div className="block-l">
+        <div className="info">
           <h2>{title}</h2>
           <h3>{author}</h3>
         </div>
-        <div className='btn-container'>
-          <button type='button'>Comments</button>
-          <button type='button' onClick={handleRemoveBook}>
+        <div className="btn-container">
+          <button type="button">Comments</button>
+          <button type="button" onClick={handleRemoveBook}>
             Remove
           </button>
-          <button type='button'>Edit</button>
+          <button type="button">Edit</button>
         </div>
       </div>
     </div>
